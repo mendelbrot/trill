d7f1482099fd6849a3cf596221aad3e953227d3b
Greg <greg@fedora.none>
2025-12-02 03:26:13 -0800

    some stiling

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class App extends StatelessWidget {
       child: MaterialApp(
         title: 'Trill',
         theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         ),
         home: HomeStack(),
       ),
@@ -28,9 +28,7 @@ class AppState extends ChangeNotifier {
   List<Widget> timers = [];
 
   void addNewTimer() {
-    timers.add(
-      SingleTimer(key: UniqueKey()),
-    );
+    timers.add(SingleTimer(key: UniqueKey()));
     notifyListeners();
   }
 
@@ -58,8 +56,8 @@ class HomeStack extends StatelessWidget {
 
     return Scaffold(
       body: ReorderableListView(
-        header: IconButton(
-          icon: const Icon(Icons.add),
+        header: ElevatedButton(
+          child: const Icon(Icons.add),
           onPressed: () {
             appState.addNewTimer();
           },
@@ -69,6 +67,15 @@ class HomeStack extends StatelessWidget {
       ),
     );
   }
+}
+
+String secondsToTimeString(int timeInSeconds) {
+  final duration = Duration(seconds: timeInSeconds);
+  String twoDigits(int n) => n.toString().padLeft(2, '0');
+  final hours = twoDigits(duration.inHours);
+  final minutes = twoDigits(duration.inMinutes.remainder(60));
+  final seconds = twoDigits(duration.inSeconds.remainder(60));
+  return "$hours:$minutes:$seconds";
 }
 
 class SingleTimer extends StatefulWidget {
@@ -131,65 +138,67 @@ class _SingleTimerState extends State<SingleTimer> {
     });
   }
 
-  String get _timerString {
-    final duration = Duration(seconds: _seconds);
-    String twoDigits(int n) => n.toString().padLeft(2, '0');
-    final hours = twoDigits(duration.inHours);
-    final minutes = twoDigits(duration.inMinutes.remainder(60));
-    final seconds = twoDigits(duration.inSeconds.remainder(60));
-    return "$hours:$minutes:$seconds";
-  }
-
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<AppState>();
     final theme = Theme.of(context);
-    final style = theme.textTheme.displayMedium!.copyWith(
+    final timeTextStyle = theme.textTheme.displayLarge!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+    final initialTimeTextStyle = theme.textTheme.displaySmall!.copyWith(
       color: theme.colorScheme.onPrimary,
     );
 
     return Card(
       color: theme.colorScheme.primary,
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(20),
-            child: Text(_timerString, style: style),
-          ),
-          Row(
-            children: [
-              _isRunning
-                  ? IconButton(
-                      icon: const Icon(Icons.pause),
-                      color: theme.colorScheme.onPrimary,
-                      onPressed: () {
-                        pauseTimer();
-                      },
-                    )
-                  : IconButton(
-                      icon: const Icon(Icons.play_arrow),
-                      color: theme.colorScheme.onPrimary,
-                      onPressed: () {
-                        startTimer();
-                      },
-                    ),
-              IconButton(
-                icon: const Icon(Icons.replay),
-                color: theme.colorScheme.onPrimary,
-                onPressed: () {
-                  resetTimer();
-                },
-              ),
-              IconButton(
-                icon: const Icon(Icons.close),
-                color: theme.colorScheme.onPrimary,
-                onPressed: () {
-                  appState.removeTimer(widget.key!);
-                },
-              ),
-            ],
-          ),
-        ],
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(secondsToTimeString(_seconds), style: timeTextStyle),
+            Row(
+              children: [
+                OutlinedButton(
+                  onPressed: () => {},
+                  child: Text(
+                    secondsToTimeString(_initialSeconds),
+                    style: initialTimeTextStyle,
+                  ),
+                ),
+                _isRunning
+                    ? IconButton(
+                        icon: const Icon(Icons.pause),
+                        color: theme.colorScheme.onPrimary,
+                        onPressed: () {
+                          pauseTimer();
+                        },
+                      )
+                    : IconButton(
+                        icon: const Icon(Icons.play_arrow),
+                        color: theme.colorScheme.onPrimary,
+                        onPressed: () {
+                          startTimer();
+                        },
+                      ),
+                IconButton(
+                  icon: const Icon(Icons.replay),
+                  color: theme.colorScheme.onPrimary,
+                  onPressed: () {
+                    resetTimer();
+                  },
+                ),
+                IconButton(
+                  icon: const Icon(Icons.close),
+                  color: theme.colorScheme.onPrimary,
+                  onPressed: () {
+                    appState.removeTimer(widget.key!);
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
